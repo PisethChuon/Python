5fdf440bdd422cc2b61c468f0ee551f1ed96a6c0
Piseth Chuon <chuonpiseth05@gmail.com>
2025-04-09 09:13:12 +0700

    update user guid instruction

--- rename_or_move_file/reormovefile.py
+++ rename_or_move_file/reormovefile.py
@@ -3,7 +3,7 @@ from pathlib import Path
 # Old file path
 old_path = Path("/path/to/old_file.txt")
 
-# New file path /can be same directory with different name or a different folder
+# New file path (can be same directory with different name or a different folder)
 new_path = Path("/path/to/new_file.txt")  # Rename
 # new_path = Path("/new/folder/new_file.txt")  # Move
 
