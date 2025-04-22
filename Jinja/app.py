961f075e5444b36f36b25e9e35b32c7e0c336483
Piseth Chuon <chuonpiseth05@gmail.com>
2025-04-22 10:51:45 +0700

    jinja

---
+++ Jinja/app.py
@@ -0,0 +1,12 @@
+from jinja2 import Template
+
+# Create a template string
+template = Template("Hello {{ name }}!") # name
+
+# Define the data
+data = {"name": "Chanthy"}
+
+# Render template
+output = template.render(data)
+
+print(output)
