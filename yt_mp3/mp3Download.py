2415177e5272a56cd30c20919663dd5c9087696a
PisethC <chuonpiseth05@gmail.com>
2025-05-25 13:22:37 +0700

    update logic

--- yt_mp3/mp3Download.py
+++ yt_mp3/mp3Download.py
@@ -3,6 +3,7 @@ from pytube import YouTube
 url = input('Enter a video to download audio:')
 yt = YouTube(url)
 
+
 # Filter audiion stream and download the first one
 audio_stream = yt.streams.filter(only_audio=True).first()
 audio_stream.download()
