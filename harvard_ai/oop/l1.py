1d137380b354aeae9b29ef3e30a78a7f46afe593
PisethC <chuonpiseth05@gmail.com>
2025-07-27 14:23:55 +0700

    updated

--- harvard_ai/oop/l1.py
+++ harvard_ai/oop/l1.py
@@ -1,4 +1,7 @@
-# Defining class
+# Defining class and creating objects
+
+# Defining class 'Car'
+'''
 class Car:
     def __init__(self, brand, color):
         self.brand = brand
@@ -10,3 +13,23 @@ class Car:
 # Creating an object
 my_car = Car("Toyota", "Red")
 my_car.drive()
+'''
+
+# Defining class 'Student'
+''''
+class Student:
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+    
+    def show_info(self):
+        print(f"{self.name}, Grade {self.grade}")
+
+# Creating an object of student
+s1 = Student("Any", "A")
+s1.show_info()  
+'''
+
+
+
+        
