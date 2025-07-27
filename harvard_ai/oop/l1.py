88b81faa8fe9a79949c1c412e6db6e405a3f3c74
PisethC <chuonpiseth05@gmail.com>
2025-07-27 22:50:13 +0700

    updated

--- harvard_ai/oop/l1.py
+++ harvard_ai/oop/l1.py
@@ -64,7 +64,7 @@ dog.bark()   # From Dog
 '''
 
 # Polymorephsim
-
+''''
 class Cat:
     def sound(self):
         print("Meow")
@@ -76,3 +76,41 @@ class Dog:
 animals = [Cat(), Dog()]
 for a in animals:
     a.sound()
+'''
+
+# Abstraction
+''''
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+
+    def area(self):
+        return 3.14 * self.r * self.r
+    
+c = Circle(5)
+print(c.area())
+'''
+
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, r):
+        self.r = r
+
+    def area(self):
+        return 3.14 * self.r * self.r
+    
+c = Circle(5)
+print(c.area())
