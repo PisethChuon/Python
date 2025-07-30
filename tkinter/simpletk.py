f1a80df405a0c84de71d06eae274324ec88292fe
Piseth Chuon <chuonpiseth05@gmail.com>
2025-07-30 14:36:23 +0700

    tkinter

---
+++ tkinter/simpletk.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# root = Tk()
+
+# root.title("AI Traffice Light Intersection Simulation")
+# root.geometry('500x300')
+
+# root.mainloop()
+
+root = Tk()
+root.title("AI Traffic")
+root.geometry('500x300')
+
+root.mainloop()
