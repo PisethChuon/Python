ea14358c78123c0d3d877ee4f1420c1da290e8a2
Piseth Chuon <chuonpiseth05@gmail.com>
2025-07-30 15:00:42 +0700

    updated

--- tkinter/simpletk.py
+++ tkinter/simpletk.py
@@ -1,14 +1,11 @@
 from tkinter import *
-
-# root = Tk()
-
-# root.title("AI Traffice Light Intersection Simulation")
-# root.geometry('500x300')
-
-# root.mainloop()
-
+from tkinter import ttk
 root = Tk()
-root.title("AI Traffic")
-root.geometry('500x300')
 
+root.geometry('500x300')
+frm = ttk.Frame(root)
+frm.grid()
+
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.quit).grid(column=1, row=0)
 root.mainloop()
