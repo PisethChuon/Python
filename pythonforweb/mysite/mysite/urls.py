2929a755e4d5e262c7b077bcbe2e5e7e8a7dd1d8
PisethC <chuonpiseth05@gmail.com>
2025-08-05 22:47:24 +0700

    start new django

--- pythonforweb/mysite/mysite/urls.py
+++
@@ -1,8 +0,0 @@
-# mysite/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),  # Connects blog app
-]
