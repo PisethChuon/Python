3b694f0e1539c1e9fad01591f5cbef6ef7b182bd
PisethC <chuonpiseth05@gmail.com>
2026-01-15 00:54:35 +0700

    Updated

--- swift-play/main.swift
+++ swift-play/main.swift
@@ -1,3 +1,10 @@
-let num = 10
+let quotation = """
+        Even though there's whitespace to the left,
+        the actual lines aren't indented.
+            Except for this line.
+        Double quotes (") can appear without being escaped.
 
-print(num)
+        I still have pieces of fruit.
+        """
+
+print(quotation)
