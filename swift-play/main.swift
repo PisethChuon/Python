40d27299d08849ffd44a06b4be4101eeb3253435
PisethChuon <chuonpiseth05@gmail.com>
2026-01-20 00:08:10 +0700

    Updated

--- swift-play/main.swift
+++ swift-play/main.swift
@@ -1,1 +1,10 @@
-print("Hello, Swift Playgrounds!")
+let quotation = """
+        Even though there's whitespace to the left,
+        the actual lines aren't indented.
+            Except for this line.
+        Double quotes (") can appear without being escaped.
+
+        I still have pieces of fruit.
+        """
+
+print(quotation)
